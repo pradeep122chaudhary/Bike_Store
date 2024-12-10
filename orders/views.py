2c1247b481b4d24ca23a1174dad464de629b92fd
Mohan Nishad <mohan.nishad@wastelink.co>
2024-12-10 13:39:43 +0530

    header footer done

--- orders/views.py
+++ orders/views.py
@@ -20,3 +20,31 @@ class Dashboard(View):
     def post(self, request):
         return render(request, self.template)
     
+# from django.views import View
+# from django.shortcuts import render
+# from os import path
+
+# from review.models import BankNiftyReview
+
+
+# class Dashboard(View):
+#     def get(self,request):
+#         trades_summary=BankNiftyReview.objects.values().order_by('-inventory_on')
+#         context={
+#             'trades_summary':trades_summary
+#         }
+#         return render(request,path.join('core','dashboard.html'),context)
+
+
+
+# def contact(request):
+
+#     return render(request,path.join('core','contact.html'))
+
+# def about(request):
+
+#     return render(request,path.join('core','about.html'))
+
+# def custom_404_view(request, exception):
+#     print('404.html exel')
+#     return render(request,path.join('core','404.html'), status=404)
